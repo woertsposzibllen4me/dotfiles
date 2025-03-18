b403e549be6c8e70e98cea979e5b52c6ed455f2c
Loïc Villet <villet.loic@gmail.com>
2025-03-18 17:55:21 +0100

    reduce terminal padding

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,12 @@ config.initial_cols = 120
 config.initial_rows = 30
 config.enable_kitty_keyboard = true
 config.enable_kitty_graphics = true
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 require("wezterm").on("format-window-title", function()
   return "Wezterm"
