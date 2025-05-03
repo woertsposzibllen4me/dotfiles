6389155cab7dcf0a6fd3be4cac6287626911f79c
Loïc Villet <villet.loic@gmail.com>
2025-05-03 23:06:55 +0200

    Cleanup and refactor pwsh profile

--- windows/scripts/pwsh/Microsoft.PowerShell_profile.ps1
+++ windows/scripts/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,72 +1,44 @@
-# Enable bracketed paste mode
-[Console]::Write("`e[?2004h")
-
-# Modules
-Import-Module Terminal-Icons
-Import-Module PSFzf
-
-# Constants
-$global:MegaScriptPath = "$env:USERPROFILE\MyMegaScript"
+## Constants
 $claudeApiKey = Get-Content -Path "$env:USERPROFILE\MyDocuments\API Keys\Anthropic\loïc-onboarding-api-key.txt" -Raw
 
-# Environment variables
+## Environment variables
 $env:PYTHONIOENCODING="utf-8"
 $env:ANTHROPIC_API_KEY = $claudeApiKey.Trim()
 
-# Importing my custom modules
+## Modules
+Import-Module Terminal-Icons
+Import-Module PSFzf
+Import-Module posh-git
+
+## Custom modules
 Import-Module "C:\Users\ville\myfiles\dotfiles\windows\scripts\pwsh\Cut-Paste.psm1"
 Set-Alias -Name cut -Value Move-ItemToBuffer
 Set-Alias -Name paste -Value Restore-ItemFromBuffer
 
-# Path additions
+## Path additions
 $env:PATH += ";C:\Users\ville\myfiles\dotfiles\windows\scripts\batch\"
 $env:Path += ";C:\Users\ville\myfiles\programs\PROGRAMS_ON_PATH\"
 $env:PATH = "$env:USERPROFILE\scoop\shims;$env:PATH"
 
-# General options
+## PSReadLine options
 Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineKeyHandler -Chord 'U' -Function Redo -ViMode Command
 Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineKeyHandler -Chord 'U' -Function Redo -ViMode Command
 Set-PSReadLineKeyHandler -Chord 'Ctrl+g' -Function AcceptNextSuggestionWord
 Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function AcceptSuggestion
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-if (Get-Module -ListAvailable -Name posh-git) {
-  # Import posh-git module
-  Import-Module posh-git
-} else {
-  # If posh-git is not installed, install it
-  Write-Host "posh-git is not installed. Installing..."
-  Install-Module posh-git -Scope CurrentUser -Force
-  Import-Module posh-git
-}
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
 
-function Invoke-FzfCd {
-  # navigate to the directory of selected file in fzf
-  $selected = fzf
 
-  if (-not $selected) {
-    Write-Host "No selection made."
-    return
-  }
-
-  if (Test-Path -Path $selected -PathType Container) {
-    z $selected
-  } else {
-    $parent = Split-Path -Path $selected -Parent
-    z $parent
-  }
-}
-
+## Custom functions
 function Set-PythonPath {
   $env:PYTHONPATH = (Get-Location).Path
   Write-Output "PYTHONPATH set to: $env:PYTHONPATH"
 }
 
 function Enter-MegaScriptEnvironment {
-  Set-Location $MegaScriptPath
+  Set-Location "C:\Users\ville\MyMegaScript\"
   Set-PythonPath
   .\venv\Scripts\activate
 }
@@ -135,10 +107,9 @@ function Start-NvimBugRepro {
   nvim -u $normalizedConfigPath
 }
 
-# Function aliases
+## Function aliases
 Set-Alias -Name spp -Value Set-PythonPath
 Set-Alias -Name eme -Value Enter-MegaScriptEnvironment
-Set-Alias -Name c -Value Clear-Host
 Set-Alias -Name dsize -Value Get-DirectorySize
 Set-Alias -Name cpath -Value Copy-PathToClipboard
 Set-Alias -Name rel -Value Update-Profile
@@ -148,14 +119,14 @@ Set-Alias -Name lgcfg -Value Edit-Lazygit-Config
 Set-Alias -Name zf -Value Invoke-FzfCd
 Set-Alias -Name virepro -Value Start-NvimBugRepro
 
-# Path aliases
+## Exec aliases
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name ex -Value explorer
 Set-Alias -Name d -Value dir
 Set-Alias -Name wh -Value where.exe
 
-# Location aliases (these are fine as-is since they're not cmdlets)
+## Location aliases
 function ahk{
   Set-Location "C:\Users\ville\myfiles\dotfiles\scripts\autohotkey"
 }
@@ -184,26 +155,8 @@ function my {
   Set-Location "C:\Users\ville\myfiles"
 }
 
-function prompt {
-  # Call the Starship prompt
-  &starship prompt --status=$LASTEXITCODE --jobs=(Get-Job | Measure-Object).Count
-}
-
-$prompt = ""
-function Invoke-Starship-PreCommand {
-  $current_location = $executionContext.SessionState.Path.CurrentLocation
-  if ($current_location.Provider.Name -eq "FileSystem") {
-    $ansi_escape = [char]27
-    $provider_path = $current_location.ProviderPath -replace "\\", "/"
-    $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
-  }
-  $host.ui.Write($prompt)
-}
-
-$env:PATH += ";C:\Program Files\starship\bin"
+## Starship
 Invoke-Expression (&starship init powershell)
 
-# Disable bracketed paste mode when exiting PowerShell
-$null = Register-EngineEvent PowerShell.Exiting -Action { [Console]::Write("`e[?2004l") }
-
+## Zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
