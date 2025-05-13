de2158fc83303a966775808cfeaa2227a83435df
Loïc Villet <villet.loic@gmail.com>
2025-05-13 02:27:06 +0200

    (browser) Add surfingkeys config

---
+++ browser/surfingkeys/settings.js
@@ -0,0 +1,12 @@
+// Rebind J and K for tab navigation
+api.map('K', 'R');
+api.map('J', 'E');
+
+// Rebind H and L for history navigation
+api.map('H', 'S');
+api.map('L', 'D');
+
+// Unbind some nonsense
+api.iunmap('<Ctrl-a>');
+api.unmap('<Ctrl-h>');
+
